a7423847305142cfc42e81d33f27f0e2c392047d
gyuudonumai <109081447+gyuudonumai@users.noreply.github.com>
2022-10-24 18:13:52 +0900

    gggggggggggggg

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
 const deen = () => {
   document.getElementById("start_audio").currentTime = 0; //連続クリックに対応
   document.getElementById("start_audio").play();
+};
+const whatName = () => {
+  canvas.width = 900;
+  canvas.height = 400;
+  canvas.setAttribute(
+    "style",
+    "display:block;margin:auto;background-color: #ffffff"
+  );
+  console.log("hey");
 };
 var epil =
   "第一次世界大戦後混沌としていたロシア帝国にある男が現れた。" +
@@ -10,9 +21,11 @@ var epil =
 document.querySelector("#start").addEventListener("click", (e) => {
   e.preventDefault();
   deen();
+  whatName();
 });
 
 window.onload = function () {
+  deen();
   Swal.fire({
     html: `<h1  style="color:#cd0000">あらすじ</h1>
            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_the_Soviet_Union.svg/800px-Flag_of_the_Soviet_Union.svg.png" alt="epilo" style="height:29%"></img>
